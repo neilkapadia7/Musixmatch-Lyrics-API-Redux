40ebc1c0d4017dadb89ff1e9fbf15e33b70ecd1e
neilkapadia7 <neilkapadia7@gmail.com>
2020-03-12 23:26:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 
-function App() {
+import {Provider} from 'react-redux';
+import store from './store';
+
+const App = () => {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lyrics Mismatch App</h1>
+      <Home />
     </div>
+    </Provider>
   );
 }
 
